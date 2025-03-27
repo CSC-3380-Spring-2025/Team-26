05a63fa35b4f04c275b95b9b441a99c198d0df54
MonkeyDChico <avega16@lsu.edu>
2025-03-27 09:55:51 -0500

    Part 1 of figuring out homepage

---
+++ src/components/header.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-600 text-white">
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        <Link href="/" className="text-2xl font-bold">
+          FoodieFinds
+        </Link>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
