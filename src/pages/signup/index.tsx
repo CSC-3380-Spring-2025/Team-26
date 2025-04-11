115de10303e87385c32e8e7cde98fb4c0245c266
YSLja <jaupar1@lsu.edu>
2025-04-11 17:24:01 -0500

    User Signup Feature

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,10 +1,96 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Icons } from '@/components/ui/icons';
+import { Input } from '@/components/ui/input';
+import { useUserAuth } from '@/context/userAuthContext';
+import { UserSignIn } from '@/types';
+import { Label } from '@radix-ui/react-label';
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const initialValue: UserSignIn = {
+    email: "",
+    password: "",
+    confirmPassword: "",
+};
 
 interface ISignupProps {
 }
 
-const Signup: React.FunctionComponent<ISignupProps> = (props) => {
-    return <div>Signup</div>;
+const Signup: React.FunctionComponent<ISignupProps> = () => {
+    
+    const {googleSignIn, signUp} = useUserAuth();
+
+    const navigate = useNavigate();
+
+    const [userInfo, setUserInfo] = React.useState<UserSignIn>(initialValue);
+
+    const handleGoogleSignIn = async(e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        try {
+            await googleSignIn();
+            navigate("/");
+
+        } catch (error) {
+            console.log("Error: ", error);
+        }
+    };
+
+    const handleSubmit = async(e: React.MouseEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            console.log("The user info is : ", userInfo);
+            await signUp(userInfo.email, userInfo.password);
+            navigate("/");
+        } catch (error) {
+            console.log("Error : ", error);
+        }
+    };
+
+    return (
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl">Create an account</CardTitle>
+            <CardDescription>
+              Enter your email below to create your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid grid-cols-2 gap-6">
+              <Button variant="outline">
+                <Icons.gitHub />
+                GitHub
+              </Button>
+              <Button variant="outline">
+                <Icons.google />
+                Google
+              </Button>
+            </div>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-card px-2 text-muted-foreground">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="m@example.com" />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Create account</Button>
+          </CardFooter>
+        </Card>
+      );
 };
 
 export default Signup;
