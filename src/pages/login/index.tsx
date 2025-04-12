df637f7dc619a08bd0c4faa15c776203ac0be7a9
YSLja <jaupar1@lsu.edu>
2025-04-12 00:37:31 -0500

    User Login Feature

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,123 @@
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Icons } from '@/components/ui/icons';
+import { Input } from '@/components/ui/input';
+import { useUserAuth } from '@/context/userAuthContext';
+import { UserLogIn } from '@/types';
+import { Label } from '@radix-ui/react-label';
 import * as React from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 
 interface ILoginProps {
 }
 
+const initialValue: UserLogIn = {
+    email: "",
+    password: "",
+};
+
 const Login: React.FunctionComponent<ILoginProps> = (props) => {
-    return <div>Login</div>;
+
+    const { googleSignIn, logIn } = useUserAuth();
+    const navigate = useNavigate();
+    const [userLogInInfo, setUserInfo] = React.useState<UserLogIn>(initialValue);
+    const [error, setError] = React.useState<string>("");
+
+    const handleGoogleSignIn = async (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        try {
+            await googleSignIn();
+            navigate("/");
+        } catch (error) {
+            console.log("Error : ", error);
+        }
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        
+        try {
+            console.log("The user info is : ", userLogInInfo);
+            await logIn(userLogInInfo.email, userLogInInfo.password);
+            navigate("/");
+        } catch (error) {
+            console.log("Error : ", error);
+        }
+    };
+
+    return <div className="bg-slate-800 w-full h-screen">
+    <div className="container mx-auto p-6 flex h-full">
+        <div className="flex justify-center items-center w-full">
+            <div className="max-w-sm rounded-xl border bg-card text-card-foreground shadow-sm">
+                <Card>
+                    <form onSubmit={handleSubmit}>
+                        <CardHeader className="space-y-1">
+                            <CardTitle className="text-2xl text-center mb-4">DiddyJuice Finder</CardTitle>
+                            <CardDescription>
+                                Enter your email below to create your account
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="grid gap-4">
+                            {/* Google Sign-in Button */}
+                            <div className="grid">
+                                <Button variant="outline" onClick={handleGoogleSignIn}>
+                                    <Icons.google className="mr-2 h-4 w-4" />
+                                    Google
+                                </Button>
+                            </div>
+                            <div className="relative">
+                                <div className="absolute inset-0 flex items-center">
+                                    <span className="w-full border-t" />
+                                </div>
+                                <div className="relative flex justify-center text-xs uppercase">
+                                    <span className="bg-background px-2 text-muted-foreground">
+                                        Or
+                                    </span>
+                                </div>
+                            </div>
+                            {/* Email Input */}
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email address</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    placeholder="dipesh@example.com"
+                                    value={userLogInInfo.email}
+                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                        setUserInfo({ ...userLogInInfo, email: e.target.value })
+                                    }
+                                />
+                            </div>
+                            {/* Password Input */}
+                            <div className="grid gap-2">
+                                <Label htmlFor="password">Password</Label>
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    placeholder="Password"
+                                    value={userLogInInfo.password}
+                                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                        setUserInfo({ ...userLogInInfo, password: e.target.value })
+                                    }
+                                />
+                            </div>
+                            {/* Display Error if Passwords Do Not Match */}
+                            {error && <p className="text-red-500 text-sm">{error}</p>}
+                        </CardContent>
+                        <CardFooter className="flex flex-col">
+                            <Button className="w-full" type="submit">
+                                Log In
+                            </Button>
+                            <p className="mt-3 text-sm text-center">
+                                Don't have an account? <Link to="/signup" className="text-blue-500">Sign up</Link>
+                            </p>
+                        </CardFooter>
+                    </form>
+                </Card>
+            </div>
+        </div>
+    </div>
+</div>;
 };
 
 export default Login;
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -6,7 +6,7 @@ import { useUserAuth } from '@/context/userAuthContext';
 import { UserSignIn } from '@/types';
 import { Label } from '@radix-ui/react-label';
 import * as React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const initialValue: UserSignIn = {
     email: "",
@@ -14,33 +14,31 @@ const initialValue: UserSignIn = {
     confirmPassword: "",
 };
 
-interface ISignupProps {
-}
+interface ISignupProps {}
 
 const Signup: React.FunctionComponent<ISignupProps> = () => {
-    
-    const {googleSignIn, signUp} = useUserAuth();
-
+    const { googleSignIn, signUp } = useUserAuth();
     const navigate = useNavigate();
-
     const [userInfo, setUserInfo] = React.useState<UserSignIn>(initialValue);
+    const [error, setError] = React.useState<string>("");
 
-    const handleGoogleSignIn = async(e: React.MouseEvent<HTMLElement>) => {
+    const handleGoogleSignIn = async (e: React.MouseEvent<HTMLElement>) => {
         e.preventDefault();
         try {
             await googleSignIn();
             navigate("/");
-
         } catch (error) {
-            console.log("Error: ", error);
+            console.log("Error : ", error);
         }
     };
 
-    const handleSubmit = async(e: React.MouseEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-
+        if (userInfo.password !== userInfo.confirmPassword) {
+            setError("Passwords do not match.");
+            return;
+        }
         try {
-            console.log("The user info is : ", userInfo);
             await signUp(userInfo.email, userInfo.password);
             navigate("/");
         } catch (error) {
@@ -49,48 +47,93 @@ const Signup: React.FunctionComponent<ISignupProps> = () => {
     };
 
     return (
-        <Card>
-          <CardHeader className="space-y-1">
-            <CardTitle className="text-2xl">Create an account</CardTitle>
-            <CardDescription>
-              Enter your email below to create your account
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="grid gap-4">
-            <div className="grid grid-cols-2 gap-6">
-              <Button variant="outline">
-                <Icons.gitHub />
-                GitHub
-              </Button>
-              <Button variant="outline">
-                <Icons.google />
-                Google
-              </Button>
+        <div className="bg-slate-800 w-full h-screen">
+            <div className="container mx-auto p-6 flex h-full">
+                <div className="flex justify-center items-center w-full">
+                    <div className="max-w-sm rounded-xl border bg-card text-card-foreground shadow-sm">
+                        <Card>
+                            <form onSubmit={handleSubmit}>
+                                <CardHeader className="space-y-1">
+                                    <CardTitle className="text-2xl text-center mb-4">DiddyJuice Finder</CardTitle>
+                                    <CardDescription>
+                                        Enter your email below to create your account
+                                    </CardDescription>
+                                </CardHeader>
+                                <CardContent className="grid gap-4">
+                                    {/* Google Sign-in Button */}
+                                    <div className="grid">
+                                        <Button variant="outline" onClick={handleGoogleSignIn}>
+                                            <Icons.google className="mr-2 h-4 w-4" />
+                                            Google
+                                        </Button>
+                                    </div>
+                                    <div className="relative">
+                                        <div className="absolute inset-0 flex items-center">
+                                            <span className="w-full border-t" />
+                                        </div>
+                                        <div className="relative flex justify-center text-xs uppercase">
+                                            <span className="bg-background px-2 text-muted-foreground">
+                                                Or
+                                            </span>
+                                        </div>
+                                    </div>
+                                    {/* Email Input */}
+                                    <div className="grid gap-2">
+                                        <Label htmlFor="email">Email address</Label>
+                                        <Input
+                                            id="email"
+                                            type="email"
+                                            placeholder="dipesh@example.com"
+                                            value={userInfo.email}
+                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                                setUserInfo({ ...userInfo, email: e.target.value })
+                                            }
+                                        />
+                                    </div>
+                                    {/* Password Input */}
+                                    <div className="grid gap-2">
+                                        <Label htmlFor="password">Password</Label>
+                                        <Input
+                                            id="password"
+                                            type="password"
+                                            placeholder="Password"
+                                            value={userInfo.password}
+                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                                setUserInfo({ ...userInfo, password: e.target.value })
+                                            }
+                                        />
+                                    </div>
+                                    {/* Confirm Password Input */}
+                                    <div className="grid gap-2">
+                                        <Label htmlFor="confirmPassword">Confirm Password</Label>
+                                        <Input
+                                            id="confirmPassword"
+                                            type="password"
+                                            placeholder="Confirm Password"
+                                            value={userInfo.confirmPassword}
+                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                                setUserInfo({ ...userInfo, confirmPassword: e.target.value })
+                                            }
+                                        />
+                                    </div>
+                                    {/* Display Error if Passwords Do Not Match */}
+                                    {error && <p className="text-red-500 text-sm">{error}</p>}
+                                </CardContent>
+                                <CardFooter className="flex flex-col">
+                                    <Button className="w-full" type="submit">
+                                        Sign Up
+                                    </Button>
+                                    <p className="mt-3 text-sm text-center">
+                                        Already have an account? <Link to="/login" className="text-blue-500">Login</Link>
+                                    </p>
+                                </CardFooter>
+                            </form>
+                        </Card>
+                    </div>
+                </div>
             </div>
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <span className="w-full border-t" />
-              </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-card px-2 text-muted-foreground">
-                  Or continue with
-                </span>
-              </div>
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" type="email" placeholder="m@example.com" />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button className="w-full">Create account</Button>
-          </CardFooter>
-        </Card>
-      );
+        </div>
+    );
 };
 
 export default Signup;
