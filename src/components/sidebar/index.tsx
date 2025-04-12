45cd3b2aec04ee010cf6bba9758c6a730faafeae
YSLja <jaupar1@lsu.edu>
2025-04-12 12:01:02 -0500

    App Layout Designed

---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface ISidebarProps {
+
+}
+
+const Sidebar: React.FunctionComponent<ISidebarProps> = (props) => {
+    return <div>Sidebar</div>;
+};
+
+export default Sidebar;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import Layout from '@/components/layout';
 
 interface IHomeProps {
 }
 
 const Home: React.FunctionComponent<IHomeProps> = (props) => {
-    return <div>Home</div>;
+    return (
+    <Layout>
+        <div>Home</div>
+    </Layout>
+    );
 };
 
 export default Home;
