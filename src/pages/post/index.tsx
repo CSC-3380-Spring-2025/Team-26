9e1e2664ad71f362b90581aab21231f3d3ce7978
YSLja <jaupar1@lsu.edu>
2025-04-13 22:53:26 -0500

    Created Post Design

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,4 +1,7 @@
 import Layout from '@/components/layout';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Textarea } from '@/components/ui/textarea';
 import * as React from 'react';
 
 interface ICreatePostProps {
@@ -7,7 +10,35 @@ interface ICreatePostProps {
 const CreatePost: React.FunctionComponent<ICreatePostProps> = (props) => {
     return (
     <Layout>
-        <div>Create Post</div>
+        <div className="flex justify-center">
+            <div className="border max-w-3xl w-full">
+                <h3 className="bg-slate-800 text-white text-center text-lg p-2">
+                    Create Post
+                </h3>
+                <div className="p-8">
+                    <form>
+                        <div className="flex flex-col">
+                            <Label className="mb-4" htmlFor="caption">
+                                Photo Caption
+                            </Label>
+                            <Textarea 
+                                className="mb-8"
+                                id="caption"
+                                placeholder="What's in your photo?"
+                            />
+                            <div className="flex flex-col">
+                                <Label className="mb-4" htmlFor="photo">
+                                    Photos
+                                </Label>
+                            </div>
+                            <Button className="mt-8 w-32" type="submit">
+                                Post
+                            </Button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </Layout>
     );
 };
