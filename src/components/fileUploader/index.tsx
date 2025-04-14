86b1ce0eb242a1fe0a89b30ba66deb5c8a4dab79
YSLja <jaupar1@lsu.edu>
2025-04-13 23:47:48 -0500

    Integrated Uploadcare to Project

---
+++ src/components/fileUploader/index.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { FileUploaderRegular } from '@uploadcare/react-uploader';
+import '@uploadcare/react-uploader/core.css';
+
+interface IFileUploaderProps {
+
+}
+
+const FileUploader: React.FunctionComponent<IFileUploaderProps> = (props) => {
+    return (
+        <div>
+            <FileUploaderRegular
+            sourceList="local, camera, gdrive"
+            cameraModes="photo, video"
+            classNameUploader="uc-light"
+            pubkey="62c8077c3253e9e01d7d"
+            />
+        </div>
+    );
+};
+
+export default FileUploader;
--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,3 +1,4 @@
+import FileUploader from '@/components/fileUploader';
 import Layout from '@/components/layout';
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
@@ -30,6 +31,7 @@ const CreatePost: React.FunctionComponent<ICreatePostProps> = (props) => {
                                 <Label className="mb-4" htmlFor="photo">
                                     Photos
                                 </Label>
+                                <FileUploader />
                             </div>
                             <Button className="mt-8 w-32" type="submit">
                                 Post
