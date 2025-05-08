a7fd16012150713844b550306afaa6f04bdbbe9e
YSLja <jaupar1@lsu.edu>
2025-05-07 20:54:21 -0500

    User Stories Componenent Aesthetic

--- src/components/stories/index.tsx
+++ src/components/stories/index.tsx
@@ -5,7 +5,12 @@ interface IStoriesProps {
 }
 
 const Stories: React.FunctionComponent<IStoriesProps> = (props) => {
-    return <div>Stories</div>;
+    return <div className="flex justify-between">
+        <img 
+            // src={} 
+            className="w-20 h-20 rounded-full border-4 border-slate-800 object-cover"
+        />
+    </div>;
 };
 
 export default Stories;
